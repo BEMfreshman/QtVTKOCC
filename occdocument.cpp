ff0381497816fe5d14fdc589928e5eade2d85215
BEMfreshman <544701552@qq.com>
2022-11-20 22:18:49 +0800

    11-20 -2

---
+++ occdocument.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 杨文露 on 2022/11/20.
+//
+
+#include "occdocument.h"
---
+++ occdocument.h
@@ -0,0 +1,14 @@
+//
+// Created by 杨文露 on 2022/11/20.
+//
+
+#ifndef QTVIEWBDF_OCCDOCUMENT_H
+#define QTVIEWBDF_OCCDOCUMENT_H
+
+
+class occdocument {
+
+};
+
+
+#endif //QTVIEWBDF_OCCDOCUMENT_H
